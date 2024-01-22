377886b66761110e94621562a69c2223f734d44c
Veronica <dv.veronica2075@gmail.com>
2024-01-22 15:57:17 -0300

    dia semana con 'switch expession'

---
+++ ContraseniaGeneratorC4.java
@@ -0,0 +1,7 @@
+/*Clase4
+        6 . Escribe un programa que genere y muestre una contraseña aleatoria. La contraseña debe tener una longitud
+        específica y contener caracteres alfanuméricos.
+*/
+
+public class ContraseniaGeneratorC4 {
+}
